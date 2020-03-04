93cd699a71e85c3d0526678b365f2ac344157b21
Ricky <rickyliew@hotmail.com>
2020-03-04 16:46:53 +1100

    Heroku push #8

--- src/containers/home.js
+++ src/containers/home.js
@@ -4,7 +4,10 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <h2>Resource Scheduler</h2>
+        <center>
+          <h2>Resource Scheduler</h2>
+          <p>The resource booking app</p>
+        </center>
       </div>
     )
   }
