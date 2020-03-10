f4ef42b4ef51e7f2f69118bf7a75eab78f951ba5
Ricky <rickyliew@hotmail.com>
2020-03-10 23:45:05 +1100

    Demo

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import calendar from "../img/calendar.png";
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         <center>
-          <h2>Resource Scheduler</h2>
+          <img src={calendar} alt="." width="45 px"></img>
+          <br/><br/>
+          <h1>Resource Scheduler</h1>
           <p>The resource booking app</p>
         </center>
       </div>
